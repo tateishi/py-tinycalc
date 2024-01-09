05e99c5976ea3beb897cd0590f62d8cee294fc41
Tateishi Tadatoshi <ishio39@gmail.com>
2024-01-10 00:37:57 +0900

    小数入力 状態遷移図

--- src/decimal.py
+++ src/decimal.py
@@ -7,8 +7,7 @@ from transitions import Machine
 class Group(Enum):
     CLEAR = 1
     NUMBER = 2
-    OPERATOR = 3
-    EQUAL = 4
+    DECIMAL = 3
     ERROR = 999
 
 
@@ -23,107 +22,77 @@ def decode(data: str) -> Token:
         return Token(Group.CLEAR, data)
     if data in "0123456789":
         return Token(Group.NUMBER, data)
-    if data in "+-*/":
-        return Token(Group.OPERATOR, data)
-    if data in "=":
-        return Token(Group.EQUAL, data)
+    if data in ".":
+        return Token(Group.DECIMAL, data)
     return Token(Group.ERROR, data)
 
 
 @dataclass
-class TinyCalc:
-    disp: float = 0
-    temp: float = 0
-    operator: str | None = None
-
-    def _first_number(self, token: Token) -> None:
-        self.disp = int(token.data)
-
-    def _rest_number(self, token: Token) -> None:
-        self.disp = self.disp * 10 + int(token.data)
-
-    def _op(self, token: Token) -> None:
-        if self.operator == "+":
-            self.disp = self.temp + self.disp
-            self.temp = self.disp
-        if self.operator == "-":
-            self.disp = self.temp - self.disp
-            self.temp = self.disp
-        if self.operator == "*":
-            self.disp = self.temp * self.disp
-            self.temp = self.disp
-        if self.operator == "/":
-            self.disp = self.temp / self.disp
-            self.temp = self.disp
-        if self.operator == None:
-            self.temp = self.disp
+class DecimalNumber:
+    reg: float = 0
+    dec: float = 0.1
 
     def on_clear(self, token: Token) -> None:
-        self.disp = 0
-        self.temp = 0
-        self.operator = None
-
-    def on_number(self, token: Token) -> None:
-        if self.state in ["s0", "s2"]:
-            self._first_number(token)
-        else:
-            self._rest_number(token)
+        self.reg = 0
+        self.dec = 0.1
 
     def on_number_1(self, token: Token) -> None:
-        self._first_number(token)
+        self.reg = int(token.data)
 
     def on_number_2(self, token: Token) -> None:
-        self._rest_number(token)
+        self.reg = self.reg * 10 + int(token.data)
 
-    def on_operator(self, token: Token) -> None:
-        self._op(token)
-        self.operator = token.data
+    def on_number_3(self, token: Token) -> None:
+        self.reg = self.reg + int(token.data) * self.dec
+        self.dec = self.dec * 0.1
 
-    def on_equal(self, token: Token) -> None:
-        self._op(token)
-        self.operator = None
+    def noop(self, token: Token) -> None:
+        pass
 
 
-def run(model: TinyCalc, machine: Machine, events: str) -> None:
+
+def run(model: DecimalNumber, machine: Machine, events: str) -> None:
+    import re
     tokens = [decode(s) for s in events]
 
     for token in tokens:
         if token.group == Group.CLEAR:
             model.clear(token)
         if token.group == Group.NUMBER:
-            import re
-
             trigger = next(
                 s
                 for s in machine.get_triggers(model.state)
                 if re.search(r"num[0-9]+", s)
             )
             model.trigger(trigger, token)
-        if token.group == Group.OPERATOR:
-            model.op(token)
-        if token.group == Group.EQUAL:
-            model.eq(token)
+        if token.group == Group.DECIMAL:
+            trigger = next(
+                s
+                for s in machine.get_triggers(model.state)
+                if re.search(r"dec[0-9]+", s)
+            )
+            model.trigger(trigger, token)
         print(model.state, token, model)
 
 
 def main():
     from pprint import pprint
 
-    states = ["s0", "s1", "s2", "err"]
+    states = ["init", "s0", "s1", "err"]
     transitions = [
-        dict(trigger="clear", source="*", dest="s0", before="on_clear"),
-        #        dict(trigger="num", source="*", dest="s1", before="on_number"),
-        dict(trigger="num1", source=["s0", "s2"], dest="s1", before="on_number_1"),
-        dict(trigger="num2", source="s1", dest="s1", before="on_number_2"),
-        dict(trigger="op", source="s1", dest="s2", before="on_operator"),
-        dict(trigger="eq", source="s1", dest="s2", before="on_equal"),
+        dict(trigger="clear", source="*", dest="init", before="on_clear"),
+        dict(trigger="num1", source="init", dest="s0", before="on_number_1"),
+        dict(trigger="num2", source="s0", dest="=", before="on_number_2"),
+        dict(trigger="num3", source="s1", dest="=", before="on_number_3"),
+        dict(trigger="dec1", source="init", dest="s1", before="noop"),
+        dict(trigger="dec2", source="s0", dest="s1", before="noop"),
     ]
-    model = TinyCalc()
+    model = DecimalNumber()
     machine = Machine(
         model=model, states=states, transitions=transitions, initial=states[0]
     )
 
-    run(model, machine, "c1+10+100=")
+    run(model, machine, "c19.123")
 
 
 if __name__ == "__main__":
